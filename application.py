d70c9116b53b9a1d8b1fb400c26916f837bde080
Ralst0n <pghRalston@gmail.com>
2018-07-15 15:26:19 -0400

    add book detail pages and api routes

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
+import requests
+
 from decouple import config
-from flask import Flask, redirect, render_template, request, session, url_for 
+from flask import Flask, jsonify, redirect, render_template, request, session, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -23,7 +25,7 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    if "username" in session:
+    if session.get("username"):
         return redirect(url_for('search'))
     return redirect(url_for('login'))
 
@@ -32,7 +34,7 @@ def signup(message=""):
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        
+
         # return an error message if either username or password isn't long enough
         if bad_cred(username, password):
             message = "Username and password can't match and must be longer than 3 characters"
@@ -40,7 +42,7 @@ def signup(message=""):
 
         if db.execute("SELECT username from users WHERE username = :username", {'username': username}).rowcount > 0:
             message = "Username is already taken. If it's yours please go to login page"
-        
+
 
         if message:
             return render_template("signup.html", destination="signup", message=message, other="login")
@@ -71,8 +73,16 @@ def login(message=""):
         message = "Invalid credentials"
     return render_template("login.html", destination="login", link=url_for("signup"), message=message )
 
+@app.route("/logout", methods=['GET'])
+def logout():
+    session['username'] = ''
+    return render_template("logout.html")
+
 @app.route("/search", methods=['GET'])
 def search(results=[], num_results=None):
+    # Check logged in
+    if not session.get("username"):
+        return redirect(url_for('login'))
     # if a search was performed
     if request.args.get("type") and request.args.get("q"):
         stype = request.args.get("type")
@@ -92,27 +102,71 @@ def search(results=[], num_results=None):
 
 @app.route("/search/<string:isbn>", methods=["GET", "POST"])
 def result(isbn):
-    if request.method == "POST":
-        rating =request.form.get("rating")
-        comment = request.form.get("comment")
-        
-        user = db.execute("SELECT * from users WHERE username = :username", {"username": session['username']}).fetchone()
-        user_id = user.id
+    # Check logged in
+    # Check logged in
+    if not session.get("username"):
+        return redirect(url_for('login'))
 
-        db.execute("INSERT INTO reviews (book_isbn, rating, comment, user_id) VALUES (:isbn, :rating, :comment, :user_id)",
-                {"isbn": isbn, "rating":rating, "comment":comment, "user_id":user_id})
-        db.commit()
-    if db.execute("SELECT * FROM books LEFT JOIN reviews ON books.isbn = reviews.book_isbn WHERE isbn = :isbn", {"isbn": isbn} ).rowcount == 0:
+    if request.method == "POST":
+        # if user hasn't reviewed book yet
+        if db.execute("SELECT * FROM reviews WHERE book_isbn = :isbn AND user_id IN (SELECT id FROM users WHERE username = :username)", {"isbn":isbn, "username": session['username']}).rowcount == 0:
+            rating =request.form.get("rating")
+            comment = request.form.get("comment")
+
+            user = db.execute("SELECT * from users WHERE username = :username", {"username": session['username']}).fetchone()
+            user_id = user.id
+
+            db.execute("INSERT INTO reviews (book_isbn, rating, comment, user_id) VALUES (:isbn, :rating, :comment, :user_id)",
+                    {"isbn": isbn, "rating":rating, "comment":comment, "user_id":user_id})
+            db.commit()
+    if db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn} ).rowcount == 0:
         return render_template("search.html")
-    book = db.execute("SELECT * FROM books LEFT JOIN reviews ON books.isbn = reviews.book_isbn WHERE isbn = :isbn", {"isbn": isbn} ).fetchone()
+    book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn} ).fetchone()
     # Get all the reviews for the book. Join the users table as well so we can stick names on each review.
     reviews = db.execute("SELECT * FROM reviews LEFT JOIN users ON users.id = reviews.user_id WHERE book_isbn = :isbn AND comment IS NOT NULL ", {"isbn": isbn}).fetchall()
-    
+
+    # Get reviews from good reads
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": config("GOODREADS_APIKEY"), "isbns": isbn})
+    goodreads= res.json()
+    gr_count = goodreads['books'][0]['work_ratings_count']
+    gr_score = goodreads['books'][0]['average_rating']
+
+
+    # Get summary from google books
+    res = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn={isbn}&key={config('GOOGLE_APIKEY')}")
+    google = res.json()
+    description = google['items'][0]['volumeInfo']['description']
+    image = google['items'][0]['volumeInfo']['imageLinks']['thumbnail']
     # show reviewed page if user alerady reaviewed the book
     if db.execute("SELECT * FROM reviews WHERE book_isbn = :isbn AND user_id IN (SELECT id FROM users WHERE username = :username)", {"isbn":isbn, "username": session['username']}).rowcount == 0:
         reviewed = False
     else:
         reviewed = True
-    return render_template("book.html", book=book, reviews=reviews, reviewed=reviewed)
+    return render_template("book.html", book=book, reviews=reviews, reviewed=reviewed, gr_count =gr_count, gr_score=gr_score,
+                            description=description, image=image)
 
+@app.route("/api/<string:isbn>", methods=["GET"])
+def api(isbn):
+    if db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn} ).rowcount == 0:
+        return render_template("404.html")
+    book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn} ).fetchone()
 
+    # Get reviews from good reads
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": config("GOODREADS_APIKEY"), "isbns": isbn})
+    goodreads= res.json()
+    gr_count = goodreads['books'][0]['work_ratings_count']
+    gr_score = goodreads['books'][0]['average_rating']
+
+    json = {
+        "title": book.title,
+        "author": book.author,
+        "year": book.year,
+        "isbn": book.isbn,
+        "review_count":gr_count,
+        "average_score": gr_score
+    }
+    return jsonify(json)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
